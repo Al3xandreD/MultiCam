ce4cc368cf58755299e7dc8e740414952a4a5bbe
Rayane GETA <rayane.geta@gmail.com>
2023-12-05 10:50:37 +0100

    wifi code test

--- ESP32 Cam/ESP32_Cam/src/ESP32_Cam.ino
+++ ESP32 Cam/ESP32_Cam/src/ESP32_Cam.ino
@@ -2,9 +2,9 @@
 #include <WiFi.h>
 #include <esp32cam.h>
  
-const int WIFI_CHOICE = 0;
-const char* WIFI_SSID[] = {"HUAWEI nova 3i", "SSID"};
-const char* WIFI_PASS[] = {"12345678","WIFI_PASS"};
+const int WIFI_CHOICE = 1;
+const char* WIFI_SSID[] = {"HUAWEI nova 3i", "Redmi Note 8 Pro"};
+const char* WIFI_PASS[] = {"12345678","12abc345"};
  
 WebServer server(80);
 
---
+++ ESP32 Cam/ESP32_SimpleWifiConnection/src/ESP32_SimpleWifiConnection.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+
+
+const int WIFI_CHOICE = 1;
+const char* WIFI_SSID[] = {"HUAWEI nova 3i", "Redmi Note 8 Pro"};
+const char* WIFI_PASS[] = {"12345678","12abc345"};
+const char* ssid = WIFI_SSID[WIFI_CHOICE];
+const char* password = WIFI_PASS[WIFI_CHOICE];
+
+void setup(){
+    Serial.begin(115200);
+    delay(1000);
+
+    WiFi.mode(WIFI_STA); //Optional
+    WiFi.begin(ssid, password);
+    Serial.println("\nConnecting");
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(100);
+    }
+
+    Serial.println("\nConnected to the WiFi network");
+    Serial.print("Local ESP32 IP: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop(){}
