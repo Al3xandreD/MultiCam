dccfadcce1a4e2bcb57abcd8cb78eb554edb5fb3
Rayane GETA <rayane.geta@gmail.com>
2023-12-12 11:59:44 +0100

    modifs

--- ESP32 Cam/ESP32_Cam/src/ESP32_Cam.ino
+++ ESP32 Cam/ESP32_Cam/src/ESP32_Cam.ino
@@ -5,7 +5,7 @@
 int L_WEIGHT_BUTTON_PIN = 14;
 int H_WEIGHT_BUTTON_PIN = 2;
 int wifi_choice = 0;
-const char* WIFI_SSID[] = {"dlinkMALEK 2", "HUAWEI nova 3i", "Redmi Note 8 Pro", "Galaxy S21 Ultra 5G3443"};
+const char* WIFI_SSID[] = {"dlinkMALEK", "HUAWEI nova 3i", "Redmi Note 8 Pro", "Galaxy S21 Ultra 5G3443"};
 const char* WIFI_PASS[] = {"felmw98786", "12345678","12345678", ""};
 
 WebServer server(80);
