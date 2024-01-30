eff529bf02768614da5656d2f64c2449b5275fd7
Rayane GETA <rayane.geta@gmail.com>
2024-01-30 18:07:49 +0100

    ESP32 Auto refresh

--- ESP32 Cam/ESP32_Cam_auto/.pio/build/esp32cam/libc4d/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32cam/libc4d/WebServer/detail/mimetable.cpp.o: \
- C:/Users/rayou/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/rayou/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/rayou/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
